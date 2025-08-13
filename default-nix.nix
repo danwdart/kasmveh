7c0cdd9480946a2f9dae939d64edcf4fd7e8d44f
Dan Dart <git@dandart.co.uk>
2025-08-13 02:12:19 +0100

    lint

--- default-nix.nix
+++ default-nix.nix
@@ -1,15 +1,15 @@
 {
   nixpkgs ? import <nixpkgs> {},
   haskell-tools ? import (builtins.fetchTarball "https://github.com/danwdart/haskell-tools/archive/master.tar.gz") {
-    nixpkgs = nixpkgs;
-    compiler = compiler;
+    inherit nixpkgs;
+    inherit compiler;
   },
   compiler ? "ghc912"
 }:
 let
   gitignore = nixpkgs.nix-gitignore.gitignoreSourcePure [ ./.gitignore ];
   tools = haskell-tools compiler;
-  lib = nixpkgs.pkgs.haskell.lib;
+  inherit (nixpkgs.pkgs.haskell) lib;
   myHaskellPackages = nixpkgs.pkgs.haskell.packages.${compiler}.override {
     overrides = self: super: rec {
       # Don't check necessarily until we can include non-Haskell stuff for tests
@@ -22,7 +22,7 @@ let
     ];
     shellHook = ''
       gen-hie > hie.yaml
-      for i in $(find -type f | grep -v "dist-*"); do krank $i; done
+      for i in $(find . -type f | grep -v "dist-*"); do krank $i; done
     '';
     # https://github.com/NixOS/nixpkgs/issues/369527
     buildInputs = tools.defaultBuildTools ++ [ nixpkgs.gettext nixpkgs.nodejs_20 nixpkgs.php82 /* nixpkgs.tinycc */ ];
@@ -32,5 +32,5 @@ let
   in
 {
   inherit shell;
-  kasmveh = lib.justStaticExecutables (myHaskellPackages.kasmveh);
+  kasmveh = lib.justStaticExecutables myHaskellPackages.kasmveh;
 }
